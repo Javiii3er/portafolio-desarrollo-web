7829c2e40f354400d51c246123d2f997ec96afa6
Javiii3er <javierjoslurp@gmail.com>
2025-07-27 21:27:31 -0600

    Personalización inicial del portafolio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mi Portafolio Académico</h1>
+        <p>Bienvenido a mi repositorio de trabajos universitarios</p>
       </header>
     </div>
   );
